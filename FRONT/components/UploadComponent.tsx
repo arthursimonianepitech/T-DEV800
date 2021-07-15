a0ab45164513b3b7431dee787ff4d8def8dbf333
Patrick <patrickdev.benzidane@gmail.com>
2021-07-15 16:33:53 +0200

    Clean FRONT

--- FRONT/components/UploadComponent.tsx
+++ FRONT/components/UploadComponent.tsx
@@ -2,13 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Button, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
-import GetPixelColor from 'react-native-get-pixel-color';
-import PixelColor from 'react-native-pixel-color';
-
+import GetPixelColor from 'react-native-get-pixel-color'; 
 
 export default function UploadComponent() {
   const [image, setImage] = useState(null);
- 
 
   useEffect(() => {
     (async () => {
@@ -20,16 +17,6 @@ export default function UploadComponent() {
       }
     })();
   }, []);
-
-
-  const getPixelImage = () => {
-    PixelColor.getHex(image, { x, y }).then((color) => {
-      // #000000
-    }).catch((err) => {
-      // Oops, something went wrong. Check that the filename is correct and
-      // inspect err to get more details.
-    });
-  }
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -50,7 +37,6 @@ export default function UploadComponent() {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      <Button title="Button" onPress={getPixelImage} />
     </View>
   );
 }
